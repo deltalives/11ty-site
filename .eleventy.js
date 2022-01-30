170b371d839e42a5bd9afbbe545fbebd7ed3bb85
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2022-01-30 16:11:09 +0530

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,34 @@
+const htmlmin = require("html-minifier")
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addWatchTarget('./tailwind.config.js')
+  eleventyConfig.addWatchTarget('./src/assets/css/tailwind.css')
+  eleventyConfig.addPassthroughCopy({
+    './node_modules/alpinejs/dist/cdn.js': './assets/js/alpine.js',
+    './src/assets/js/*': './assets/js/',
+    './src/assets/css/extra.css': './style.css',
+  })
+
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if (outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      })
+      return minified
+    }
+    return content
+  })
+
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      data: '_data',
+      output: `_site`
+    }
+  }
+
+}
